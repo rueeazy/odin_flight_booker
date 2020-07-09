23a3835028acd0daa9f2ce03e2ecbd707f4c1577
Christian Ruiz <santacruz.crm@gmail.com>
2020-07-09 13:32:43 -0700

    Migrate airport/flight databases

--- db/migrate/20200709194845_create_flights.rb
+++ db/migrate/20200709194845_create_flights.rb
@@ -1,6 +1,10 @@
 class CreateFlights < ActiveRecord::Migration[6.0]
   def change
     create_table :flights do |t|
+      t.string :start_airport
+      t.string :end_airport
+      t.datetime :departure_time
+      t.integer :flight_duration
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Airport.delete_all
+airports = Airport.create([{ airport_code: 'SFO' }, { airport_code: 'NYC' }, { airport_code: 'LAX' }, 
+{ airport_code: 'LAS' }, { airport_code: 'DEN' }, { airport_code: 'SEA' }, {airport_code: 'PHX' }])
